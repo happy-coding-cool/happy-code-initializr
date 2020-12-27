a31f24968701e1a42471dfe7504fab6a97f04d8e
pengzhenchen <pzsoftchen@163.com>
2020-12-27 12:40:06 +0800

    feat: 添加脚手架入口类

---
+++ src/main/java/cool/happycoding/code/initializr/api/v1/form/HappyCodeForm.java
@@ -0,0 +1,14 @@
+package cool.happycoding.code.initializr.api.v1.form;
+
+import cool.happycoding.code.base.pojo.Form;
+import lombok.Data;
+
+/**
+ * @ClassName HappyCodeForm
+ * @Description TODO
+ * @Author lanlanhappy
+ * @Date 2020/12/27 12:36 下午
+ */
+@Data
+public class HappyCodeForm extends Form {
+}
---
+++ src/main/java/cool/happycoding/code/initializr/api/v1/rest/HappyCodeGeneratorController.java
@@ -0,0 +1,17 @@
+package cool.happycoding.code.initializr.api.v1.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HappyCodeGeneratorController
+ * @Description happy-code 脚手架
+ * @Author lanlanhappy
+ * @Date 2020/12/27 12:34 下午
+ */
+@RestController
+@RequestMapping("happy-code")
+public class HappyCodeGeneratorController {
+
+
+}
