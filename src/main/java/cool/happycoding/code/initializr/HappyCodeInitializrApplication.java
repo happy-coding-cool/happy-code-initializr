dc95f59d453d6fc53739038498f99f152da0ca0a
pengzhenchen <pzsoftchen@163.com>
2020-12-27 12:29:19 +0800

    init: 项目初始化

---
+++ src/main/java/cool/happycoding/code/initializr/HappyCodeInitializrApplication.java
@@ -0,0 +1,18 @@
+package cool.happycoding.code.initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description
+ *
+ * @author lanlanhappy 2020/12/27 11:11 上午
+ */
+@SpringBootApplication
+public class HappyCodeInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyCodeInitializrApplication.class, args);
+    }
+
+}
