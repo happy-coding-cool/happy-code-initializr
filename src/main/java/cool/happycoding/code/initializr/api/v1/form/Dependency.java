96ff0c85b081fc5e879d8e2cfd43e600ca862000
pengzhenchen <pzsoftchen@163.com>
2020-12-28 22:18:00 +0800

    feat: 完善脚手架逻辑

---
+++ src/main/java/cool/happycoding/code/initializr/api/v1/form/Dependency.java
@@ -0,0 +1,17 @@
+package cool.happycoding.code.initializr.api.v1.form;
+
+import cool.happycoding.code.base.pojo.Form;
+import lombok.Data;
+
+/**
+ * @ClassName Dependency
+ * @Description 依赖
+ * @Author lanlanhappy
+ * @Date 2020/12/28 10:13 下午
+ */
+@Data
+public class Dependency extends Form {
+
+    private String dependency;
+
+}
---
+++ src/main/java/cool/happycoding/code/initializr/api/v1/form/ProjectMetadata.java
@@ -0,0 +1,21 @@
+package cool.happycoding.code.initializr.api.v1.form;
+
+import cool.happycoding.code.base.pojo.Form;
+import lombok.Data;
+
+/**
+ * @ClassName ProjectMetadata
+ * @Description 项目元数据
+ * @Author lanlanhappy
+ * @Date 2020/12/28 10:10 下午
+ */
+@Data
+public class ProjectMetadata extends Form {
+
+    private String  group;
+    private String  artifact;
+    private String  name;
+    private String  description;
+    private String  projectName;
+    private String  javaVersion;
+}
