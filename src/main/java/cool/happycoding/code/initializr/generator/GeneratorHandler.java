ae9483939cd108e608f0a89b26215e01b0600842
pengzhenchen <pzsoftchen@163.com>
2021-01-13 08:36:36 +0800

    feat: 完善代码生成逻辑

--- src/main/java/cool/happycoding/code/initializr/generator/GeneratorHandler.java
+++ src/main/java/cool/happycoding/code/initializr/generator/GeneratorHandler.java
@@ -3,8 +3,12 @@ package cool.happycoding.code.initializr.generator;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ZipUtil;
 import cool.happycoding.code.initializr.api.v1.form.HappyCodeForm;
+import cool.happycoding.code.initializr.base.BaseGenerationConfiguration;
+import cool.happycoding.code.initializr.base.BaseGenerator;
+import cool.happycoding.code.initializr.generator.build.maven.MavenBuildGenerationConfiguration;
+import cool.happycoding.code.initializr.generator.build.maven.MavenBuildGenerator;
+import freemarker.template.Configuration;
 
-import javax.security.auth.login.Configuration;
 import java.io.File;
 
 /**
@@ -21,7 +25,11 @@ public class GeneratorHandler {
             FileUtil.del(file);
         }
         FileUtil.mkdir(file);
-        // TODO file 文件生成
+        // file 文件生成
+        new GeneratorChain()
+                .next(new BaseGenerator(BaseGenerationConfiguration.of(configuration, happyCodeForm)))
+                .next(new MavenBuildGenerator(MavenBuildGenerationConfiguration.of(configuration, happyCodeForm)))
+                .generator();
         File zipFile = ZipUtil.zip(new File(
                         happyCodeForm.getProjectMetadata().getArtifact() + ".zip"),
                 true, file);
