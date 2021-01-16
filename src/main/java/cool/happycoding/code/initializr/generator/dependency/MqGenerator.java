668591ce0c39f2ca740bc04ddef2d73c4260ad90
pengzhenchen <pzsoftchen@163.com>
2021-01-16 16:23:18 +0800

    reactor: 重构代码删除过度设计的类

--- src/main/java/cool/happycoding/code/initializr/generator/dependency/MqGenerator.java
+++ src/main/java/cool/happycoding/code/initializr/generator/dependency/MqGenerator.java
@@ -1,17 +1,14 @@
 package cool.happycoding.code.initializr.generator.dependency;
 
 import cool.happycoding.code.initializr.generator.Generator;
-import cool.happycoding.code.initializr.generator.dependency.mybatis.MybatisGenerationConfiguration;
 
 /**
- * @ClassName MybatisGenerator
- * @Description mybatis组件
+ * @ClassName MqGenerator
+ * @Description mq组件依赖
  * @Author lanlanhappy
  * @Date 2020/12/30 9:54 下午
  */
 public class MqGenerator implements Generator {
-
-    private MybatisGenerationConfiguration mybatisGenerationConfiguration;
 
     @Override
     public void generator() {
