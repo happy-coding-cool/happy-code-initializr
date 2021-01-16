ab8c6375c6009a4ea2a0d8062bfb5891746ebc64
pengzhenchen <pzsoftchen@163.com>
2021-01-16 17:48:17 +0800

    feat: 实现生成的rest入口逻辑

--- src/main/java/cool/happycoding/code/initializr/api/v1/rest/HappyCodeGeneratorController.java
+++ src/main/java/cool/happycoding/code/initializr/api/v1/rest/HappyCodeGeneratorController.java
@@ -1,6 +1,10 @@
 package cool.happycoding.code.initializr.api.v1.rest;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
+import cool.happycoding.code.base.exception.BizException;
 import cool.happycoding.code.initializr.api.v1.form.HappyCodeForm;
+import cool.happycoding.code.initializr.generator.GeneratorHandler;
 import freemarker.template.Configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * @ClassName HappyCodeGeneratorController
@@ -30,8 +35,23 @@ public class HappyCodeGeneratorController {
      */
     @PostMapping("start")
     public void start(@RequestBody HappyCodeForm happyCodeForm, HttpServletResponse response){
-        // TODO
-
+        File zipFile = GeneratorHandler.generator(configuration, happyCodeForm);
+        response.setContentType("application/zip");
+        // 设置下载后的文件名 以及 header
+        response.addHeader("Content-disposition",
+                "attachment;fileName=" + happyCodeForm.getProjectMetadata().getArtifact() + ".zip");
+        try {
+            InputStream fis = new BufferedInputStream(new FileInputStream(zipFile));
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            IoUtil.copy(fis, toClient);
+            toClient.close();
+            fis.close();
+        } catch (Exception e){
+            throw new BizException("9999", e.getMessage());
+        }finally {
+            // 删除文件
+            FileUtil.del(zipFile);
+        }
     }
 
 }
