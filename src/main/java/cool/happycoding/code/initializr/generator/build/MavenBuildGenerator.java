e7f5e6b61f0f96e25b8407ecfaaf1e71d8366645
pengzhenchen <pzsoftchen@163.com>
2021-03-06 11:28:24 +0800

    feat: 添加对mvnw文件的生成

--- src/main/java/cool/happycoding/code/initializr/generator/build/MavenBuildGenerator.java
+++ src/main/java/cool/happycoding/code/initializr/generator/build/MavenBuildGenerator.java
@@ -17,23 +17,22 @@ import static cool.happycoding.code.initializr.generator.GeneratorPath.GenerateF
 public class MavenBuildGenerator implements Generator{
 
     private final GenerationConfiguration generationConfiguration;
-    private final String MVNW_PATH = "/file/mvn/mvmw";
-    private final String MVNW_CMD_PATH = "/file/mvn/mvmw.cmd";
-    private final String MVN_WRAPPER_PATH = "file/mvn/wrapper";
 
     public MavenBuildGenerator(GenerationConfiguration generationConfiguration){
         this.generationConfiguration = generationConfiguration;
     }
     @Override
     public void generator() {
+        // 复制 file目录下的文件到目标目录
+        String mvnwPath = "/file/mvn/mvmw";
+        String mvnwCmdPath = "/file/mvn/mvmw.cmd";
+        String mvnWrapperPath = "file/mvn/wrapper";
+        // 复制 mvnw mvn.cmd 文件
+        FileUtil.copy(mvnwPath, generationConfiguration.getZipFilePath(), true);
+        FileUtil.copy(mvnwCmdPath, generationConfiguration.getZipFilePath(), true);
+        // 生成.mvn 文件夹 复制 wrapper文件的内容
+        FileUtil.copy(mvnWrapperPath, StrUtil.concat(false, generationConfiguration.getZipFilePath(),"/.mvn/"), true);
         // 生成pom文件
         new BaseGenerator(generationConfiguration, POM_FILE).generator();
-        // 生成.mvn 文件夹
-        // 复制 file目录下的文件到目标目录
-        // 复制 mvnw mvn.cmd 文件
-        FileUtil.copy(MVNW_PATH, generationConfiguration.getZipFilePath(), true);
-        FileUtil.copy(MVNW_CMD_PATH, generationConfiguration.getZipFilePath(), true);
-        // 复制 wrapper文件的内容
-        FileUtil.copy(MVN_WRAPPER_PATH, StrUtil.concat(false, generationConfiguration.getZipFilePath(),"/.mvn/"), true);
     }
 }
